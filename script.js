9e9a34ec45ab83e7dae4c7515812dd3970486446
Raphaelly Santos <raphaelly.santos@rebase.com.br>
2024-10-18 18:24:50 -0300

    Adjust winner message

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const possibleChoices = ['Rock', 'Paper', 'Scissor']
 const resultsDiv = document.querySelector('#results')
 const computerScoreSpan = document.querySelector('#computer-score')
 const humanScoreSpan = document.querySelector('#human-score')
-
 const resultParagraph = document.querySelector('#round-result')
 const winnerParagraph = document.querySelector("#game-result")
 
@@ -16,9 +15,9 @@ function getComputerChoice() {
 
 function displayGameWinner(humanScore, computerScore) {
   if (humanScore > computerScore) {
-    winnerParagraph.textContent = "YOU WIN!!"
+    winnerParagraph.textContent = "GAME OVER! YOU WIN!!"
   } else {
-    winnerParagraph.textContent = "YOU LOSE!!"
+    winnerParagraph.textContent = "GAME OVER! YOU LOSE!!"
   }
 }
 
@@ -51,11 +50,6 @@ function playRound(humanChoice, computerChoice) {
   isGameOver && displayGameWinner(humanScore, computerScore)
 }
 
-possibleChoices.map((choice) =>{
-  const btn = document.querySelector(`#${choice.toLowerCase()}-btn`)
-  btn.addEventListener("click", () => { playGame(choice) })
-})
-
 function playGame(choice) {
   const isGameOn = humanScore < 5 && computerScore < 5
 
@@ -63,4 +57,10 @@ function playGame(choice) {
     return playRound(choice, getComputerChoice())
   }
 }
+
+possibleChoices.map((choice) =>{
+  const btn = document.querySelector(`#${choice.toLowerCase()}-btn`)
+  btn.addEventListener("click", () => { playGame(choice) })
+})
+
 
