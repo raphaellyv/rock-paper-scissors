c463bdf16f52f77983c6e576b15752fac57d1b09
Raphaelly Santos <raphaelly.santos@rebase.com.br>
2024-10-18 16:10:22 -0300

    End game after 5 points

--- script.js
+++ script.js
@@ -13,31 +13,53 @@ function playRound(humanChoice, computerChoice) {
   const humanWins = `You win! ${ humanChoice } beats ${ computerChoice }.`
   const aTie = 'It\'s a tie!'
   let result
-
+  
+  
   if (computerChoice === humanChoice) {
     result = aTie
-  }
-  else if ((computerChoice === 'Rock' && humanChoice === 'Scissor') ||
-    (computerChoice === 'Paper' && humanChoice === 'Rock') ||
-    (computerChoice === 'Scissor' && humanChoice === 'Paper')
-  ) {
-    result = computerWins
-    computerScore+=1
-  } else {
-    result = humanWins
-    humanScore+=1
-  }
-  
-  
+  } else if ((computerChoice === 'Rock' && humanChoice === 'Scissor') ||
+  (computerChoice === 'Paper' && humanChoice === 'Rock') ||
+  (computerChoice === 'Scissor' && humanChoice === 'Paper')
+) {
+  result = computerWins
+  computerScore+=1
+} else {
+  result = humanWins
+  humanScore+=1
+}
+
+  const isGameOver = humanScore === 5 || computerScore === 5
   const resultParagraph = document.createElement("p")
   resultParagraph.textContent = result
   resultsDiv.appendChild(resultParagraph)
   console.log('human', humanScore)
   console.log('computer', computerScore)
+  
+  const winnerParagraph = document.createElement("p")
+  resultsDiv.appendChild(winnerParagraph)
+  
+  if (isGameOver) {
+    if (humanScore > computerScore) {
+      winnerParagraph.textContent = "YOU WIN!!"
+    } else {
+      winnerParagraph.textContent = "YOU LOSE!!"
+    }
+  }
   return console.log(result)
 }
 
 possibleChoices.map((choice) =>{
   const btn = document.querySelector(`#${choice.toLowerCase()}-btn`)
-  btn.addEventListener("click", () => { playRound(choice, getComputerChoice()) })
+  btn.addEventListener("click", () => { playGame(choice) })
 })
+
+function playGame(choice) {
+  const isGameOn = humanScore < 5 && computerScore < 5
+
+  if (isGameOn) {
+    return playRound(choice, getComputerChoice())
+  } else {
+    return
+  }
+}
+
